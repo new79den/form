eeb254dd910b0d42b31ed545cd4a8b93918354fb
Denis Korniychuk <new79den@gmail.com>
2017-01-25 19:00:35 +0200

    Урок 99: Two-Way Binding with ngModel (Template-Driven)

--- form/src/app/template-driven/template-driven.component.ts
+++ form/src/app/template-driven/template-driven.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'template-driven',
   templateUrl: './template-driven.component.html',
+  styles: [`
+    .ng-invalid{
+    border: 1px solid red;
+    }    
+`]
 })
-export class TemplateDrivenComponent  {
-  onSubmit(form: NgForm){
+export class TemplateDrivenComponent {
+  onSubmit(form: NgForm) {
     console.log(form);
   }
 
