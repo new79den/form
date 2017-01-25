a2e45c149a68d87910663b58a3975e4e96edcc66
Denis Korniychuk <new79den@gmail.com>
2017-01-25 19:12:44 +0200

    Урок 101: Radio Button Controls (Template-Driven)

--- form/src/app/template-driven/template-driven.component.ts
+++ form/src/app/template-driven/template-driven.component.ts
@@ -11,8 +11,14 @@ import {NgForm} from "@angular/forms";
 `]
 })
 export class TemplateDrivenComponent {
+  user = {
+    username: "",
+    email: "",
+    password: "",
+  }
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    console.log(this.user);
   }
 
 }
