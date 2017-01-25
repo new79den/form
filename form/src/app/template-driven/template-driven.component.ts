1dcc956baacc5115aed53a2c51fc6b462e9c2ed7
Denis Korniychuk <new79den@gmail.com>
2017-01-25 18:21:48 +0200

    Урок 96: Input Validation (Template-Driven)

--- form/src/app/template-driven/template-driven.component.ts
+++ form/src/app/template-driven/template-driven.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'template-driven',
   templateUrl: './template-driven.component.html',
 })
-export class TemplateDrivenComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class TemplateDrivenComponent  {
+  onSubmit(form: NgForm){
+    console.log(form);
   }
 
 }
