bed953f412b78bb06056009b97ee104cb2df5d9e
Denis Korniychuk <new79den@gmail.com>
2017-01-25 18:05:45 +0200

    Урок 95: Form Properties

---
+++ form/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { DataDrivenComponent } from './data-driven/data-driven.component';
+import { TemplateDrivenComponent } from './template-driven/template-driven.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataDrivenComponent,
+    TemplateDrivenComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ form/src/app/data-driven/data-driven.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fm-data-driven',
+  templateUrl: './data-driven.component.html',
+})
+export class DataDrivenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
