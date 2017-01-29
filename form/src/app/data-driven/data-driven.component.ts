a6b04291920f4e1755ca791c1236583331b4292c
Denis Korniychuk <new79den@gmail.com>
2017-01-29 15:15:10 +0300

    Урок 117: Shopping List Form HTML Setup (Template-Driven)

--- form/src/app/data-driven/data-driven.component.ts
+++ form/src/app/data-driven/data-driven.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {FormGroup, FormControl, Validators} from "@angular/forms";
+import {FormGroup, FormControl, Validators, FormBuilder} from "@angular/forms";
 
 @Component({
   selector: 'fm-data-driven',
@@ -9,17 +9,32 @@ export class DataDrivenComponent {
 myForm : FormGroup;
 
 
-  constructor() {
-    this.myForm = new FormGroup({
+  constructor(private formBuilder: FormBuilder) {
+   /* this.myForm = new FormGroup({
       "userData": new FormGroup({
         'username': new FormControl("Max", Validators.required),
         'email': new FormControl('', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]),
       }),
       'password': new FormControl('', Validators.required),
-    })
+    });*/
+
+    this.myForm =  formBuilder.group({
+      "userData": formBuilder.group({
+        'username': ["Max", [Validators.required , this.exampleValidator]],
+        'email': ['', [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]],
+      }),
+      'password': ['', Validators.required],
+    });
   }
 
   onSubmit(){
     console.log(this.myForm);
+  }
+
+  exampleValidator(control: FormControl): {[s: string] : boolean}{
+    if( control.value === 'Expamle'){
+      return {example: true}
+    }
+    return null;
   }
 }
